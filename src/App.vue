05d058a961947ff0200a5f1c00b8ac58fb719b4e
李富康 <2407825689@qq.com>
2024-04-16 09:02:23 +0800

    111

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-config-provider :locale="locale">
+    <HelloWorld msg="Vite + Vue" />
+  </el-config-provider>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+import plusZhCn from 'plus-pro-components/es/locale/lang/zh-cn'
+
+const locale = ref({ ...zhCn, ...plusZhCn });
+</script>
